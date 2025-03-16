b5bb6adaa97d0f85d8e5f5ef38f5e2daad27fa46
deep10086 <2201161884@qq.com>
2025-03-16 17:06:52 +0800

    Add files via upload

---
+++ FileName.c
@@ -0,0 +1,45 @@
+#include<stdio.h>
+#include<stdlib.h>
+#define ElemType char
+typedef struct TreeNode {
+	ElemType data;
+	struct TreeNode* rchi;
+		struct TreeNode* lchi;
+}TreeNode;
+typedef TreeNode* BiTree;
+char str[] = "ABDH#K###E##CFI###G#J##";
+int id = 0;
+//生成树
+void creatTree(BiTree* T) {
+	ElemType ch;
+	ch = str[id++];
+	if (ch == '#')//第一次这里弄成双引号了 
+	{
+		*T = NULL;
+	}
+	else {
+		*T = (BiTree)malloc(sizeof(TreeNode));
+		(*T)->data = ch;
+		creatTree(&(*T)->lchi);
+		creatTree(&(*T)->rchi);
+	}
+}
+//读取树
+void readTree(BiTree T){
+	if (T == NULL) {
+		return;
+	}
+	printf("%c", T->data);//前序
+	readTree(T->lchi);
+	readTree(T->rchi);
+
+}
+int main()
+{
+	BiTree T;
+	creatTree(&T);
+	readTree(T);
+	
+	printf("\n");
+	return 0;
+}
